92fb1c6c58be2cb06b51f74891ff521db4a8a0ae
Guilherme Medeiros <eu@guilhermemedeiros.com.br>
2015-01-01 14:48:58 +0100

    refactoring

--- app.js
+++ app.js
@@ -1,166 +1,229 @@
-// var cheerio = require('cheerio');
 var request = require('request').defaults({jar:true});
 var _ = require('lodash');
 var exec = require('exec');
 var fs = require('fs');
 var path = require('path');
-var chalk = require('chalk');
-var ProgressBar = require('node-progress');
+var Q = require('q');
+var chalk = require('chalk')
 
-request.post({
-	url: 'https://login.globo.com/api/authentication',
-	headers: {
-		'Content-Type': 'application/json'
-	},
-	body: {
-		email: 'medeeiros@globo.com',
-		password: 'dnamoris2',
-		serviceId: 4654
-	},
-	json: true
+var globo = (function(){
 
-}, function(error, response, html){
+	var getHash = function(videoID, resourceID){
+		var defer = Q.defer()
 
-	request.get({
-		url: 'http://globotv.globo.com/-/upa/watch_later/?per_page=100',
-	}, function(error, response, body){
-		if(!error){
-			var data = JSON.parse(body);
-
-			// data.data.forEach(function(item){})
-
-			// http://api.globovideos.com/videos/3844687/playlist/
-			// data.data[0].metadata.url_for_consumption
-			//http://security.video.globo.com/videos/3844687/hash?resource_id=3844687_wmsm&version=2.9.9.65&udid=null&player=html5
-
-
-			//http://voddownload03.globo.com/v0/01/86/4f/3842637_4f7e8bec8c9dc37d30090697149df44082f63641/3842637-web720.mp4?h=05021419935524945211522214200219244524761019N22VtuKupwVdBlJAzVpS6w&k=flash offline
-			//http://voddownload03.globo.com/v0/01/86/4f/3842637_4f7e8bec8c9dc37d30090697149df44082f63641/3842637-web720.mp4?h=0214199354187912524696K6xASAXzUkmg93HXui4qqQ&k=flash node
-			//http://voddownload03.globo.com/v0/01/86/4f/3842637_4f7e8bec8c9dc37d30090697149df44082f63641/3842637-web720.mp4?h=021419935677638573169414200220774344107476TSfDyBvS1WHlOnCZkn2VHg&k=flash //original
-			//http://voddownload03.globo.com/v0/01/86/4f/3842637_4f7e8bec8c9dc37d30090697149df44082f63641/3842637-web720.mp4?h=05021420034035172546262014200376354598391001BDqVyeDetVa1nzWG4fAuBw&k=flash //python signed
-
-
-
-			_.each(data.data, function(watch_later){
-
-				request.get({
-					url: "http://api.globovideos.com/videos/"+watch_later.resource_id+"/playlist/"
-				}, function(error, response, body){
-
-					var playlist = JSON.parse(body);
-
-					_.each(playlist.videos, function(video){
-
-						if(video.children) {
-							_.each(video.children, function(child){
-								_.each(child.resources, function(resource){
-									if(resource.height >= 720) {
-										downloadResource(video, resource, child);
-									}
-								})
-							})
-
-						} else {
-							_.each(video.resources, function(resource){
-								if(resource.height >= 720) {
-									downloadResource(video, resource);
-								}
-							})
-						}
-
-
-
-					})
-
-				})
-
-			})
-
-		}
-	})
-})
-
-
-var downloading = {};
-
-function updateStatus(){
-	var data = [];
-
-	_.each(downloading, function(download){
-		data.push(download.video.title + ' - ' + download.resource._id + ' - ' + (parseInt(download.current, 10) / parseInt(download.total, 10) * 100) + '%')
-	})
-
-	// process.stderr.moveCursor(0, data.length*-1);
-	// process.stderr.clearScreenDown();
-	// process.stderr.cursorTo(0);
-	// process.stderr.write(data.join('\n'));
-
-	console.log(data)
-}
-
-function downloadResource(video, resource, child) {
-	// console.log(video.id);
-	// console.log(resource._id);
-
-	request.get({
-		// url: "http://security.video.globo.com/videos/"+video.id+"/hash?resource_id="+resource._id+"&version=2.9.9.65&udid=null&player=html5"
-		url: "http://security.video.globo.com/videos/"+ (child.id || video.id) +"/hash?resource_id="+resource._id+"&version=2.9.9.65&player=html5"
-	}, function(error, response, body){
-
-		console.log(response.body)
-
-		var hash = (JSON.parse(body)).hash;
-
-		exec(['python', 'hash.py', hash], function(err, out, code){
-			// console.log(out);
-
-			hash = out.slice(2, -3)
-			var qs = resource.query_string_template.replace('{{hash}}', hash).replace('{{key}}', 'html5')
-			var url = resource.url + '?' + qs;
-
-			var req = request.get({url: url});
-
-				req.on('response', function(res){
-
-					if (res.headers['content-length']) {
-
-						downloading[video.id + '_ ' + resource._id] = {res: res, resource: resource, video: video, current: 0, total: res.headers['content-length']};
-
-						res.on('data', function (data) {
-							downloading[video.id + '_ ' + resource._id].current+=data.length;
-							updateStatus();
-						});
-
-						res.on('end', function () {
-							console.log('end')
-						});
-					}
-
-				})
-
-				req.on('error', function(err){
-					console.log(err)
-				})
-
-				// req.pipe(fs.createWriteStream(path.basename(resource.url)));
-
-				var folder = video.title.replace(/\//gi, '-').replace(/[\,]/gi, '')
-
-				if(child){
-					if (!fs.existsSync(folder)) {
-						fs.mkdirSync(folder)
-					}
-					req.pipe(fs.createWriteStream('./' + folder + '/' + child.title + '.mp4'));
-				} else {
-					req.pipe(fs.createWriteStream('./' + video.title + '.mp4'));
-
-				}
-
-
-			// console.log(resource)
-			// console.log(hash)
-			// console.log(url)
+		request.get({
+			url: "http://security.video.globo.com/videos/"+ videoID +"/hash?resource_id="+resourceID+"&version=2.9.9.65&player=html5"
+		}, function(error, response, body){
+			if(error){
+				defer.reject(error)
+			} else {
+				var data = JSON.parse(body);
+				defer.resolve(data.hash);
+			}
 		})
 
-	})
+		return defer.promise;
+	}
+
+	var getSignedHash = function(hash){
+		var defer = Q.defer()
+
+		exec(['python', 'hash.py', hash], function(err, out, code){
+			if(err || !out || out.length < 5) {
+				defer.reject()
+			} else {
+				defer.resolve(out.slice(2, -3))
+			}
+		})
+		return defer.promise;
+	}
+
+	var login = function(email, password, serviceId){
+		var defer = Q.defer()
+
+		request.post({
+			url: 'https://login.globo.com/api/authentication',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: {
+				email: email,
+				password: password,
+				serviceId: serviceId || 4654
+			},
+			json: true
+
+		}, function(error, response){
+			if(error){
+				defer.reject(error);
+			} else {
+				defer.resolve(response);
+			}
+		})
+
+		return defer.promise;
+	}
+
+	var getPlaylist = function(videoID){
+		var defer = Q.defer()
+
+		request.get("http://api.globovideos.com/videos/"+videoID+"/playlist/", function(error, response, body){
+			if(error){
+				defer.reject(error);
+			} else {
+				defer.resolve(JSON.parse(body));
+			}
+		})
+
+		return defer.promise;
+	}
+
+	var getIDFromURL = function(url){
+		return url.match(/\/(\d+)\/?$/)[1];
+	}
+
+	var getDownloadURL = function(video, resource){
+
+		return getHash(video.id, resource._id)
+			.then(getSignedHash)
+			.then(function(signedHash){
+				var qs_template = resource.query_string_template || 'h={{hash}}&k={{key}}';
+				var qs = qs_template.replace('{{hash}}', signedHash).replace('{{key}}', 'html5')
+
+				return resource.url + '?' + qs;
+			})
+	}
+
+	return {
+		getSignedHash: getSignedHash,
+		login: login,
+		getPlaylist: getPlaylist,
+		getIDFromURL: getIDFromURL,
+		getDownloadURL: getDownloadURL
+	}
+
+}())
+
+
+var downloadManager = (function(){
+	var _downloading = [];
+	var _progressInfo = {};
+
+	var startDownload = function(url, folder, filename){
+		var defer = Q.defer();
+		var extension = path.extname(url.split('?')[0]);
+			folder = folder.replace(/\//gi, '-').replace(/[\,]/gi, '')
+			filename = filename.replace(/\//gi, '-').replace(/[\,]/gi, '')
+
+		var destination = path.join(process.argv[3] || './', folder, filename + extension)
+		var totalSize = 0;
+		var downloaded = 0;
+		var req = request.get(url);
+			req.on('error', defer.reject);
+			req.on('response', function(response){
+				if(response.headers['content-length']) {
+
+					totalSize = response.headers['content-length'];
+
+					response.on('data', function (data) {
+						downloaded += data.length
+						_progressInfo[destination] = {
+							downloaded: downloaded,
+							totalSize: totalSize,
+							destination: destination
+						}
+						defer.notify(_progressInfo[destination]);
+					})
+
+					response.on('end', function(){
+						defer.resolve(destination)
+						_.pull(_downloading, destination);
+						logEnd(destination)
+					})
+				}
+			})
+
+		if (!fs.existsSync(path.dirname(destination))) {
+			fs.mkdirSync(path.dirname(destination))
+		}
+
+		req.pipe(fs.createWriteStream(destination));
+		_downloading.push(destination);
+		logProgress();
+
+		return defer.promise;
+	}
+
+	var logProgress = function(){
+		if(_downloading.length > 0) {
+			var hasProgress = false;
+
+			_.each(_downloading, function(item){
+				var progress = _progressInfo[item];
+				if(hasProgress = progress){
+					console.log(chalk.gray(progress.destination), parseFloat(progress.downloaded/progress.totalSize*100, 10).toFixed(2) + '%')
+				}
+			})
+
+			if(hasProgress) console.log(chalk.gray('--------------'))
+
+			setTimeout(logProgress, 500);
+		}
+	}
+
+	var logEnd = function(destination){
+		console.log(chalk.gray(destination), chalk.green('✓'))
+	}
+
+	return {
+		startDownload: startDownload
+	}
+
+}())
+
+
+if(!process.argv[2]) {
+	return console.log('URL not defined');
 }
+
+globo.login('medeeiros@globo.com', 'dnamoris2')
+	.then(function(){
+		return process.argv[2]
+	})
+	.then(globo.getIDFromURL)
+	.then(globo.getPlaylist)
+	.then(function(playlist){
+
+		_.each(playlist.videos, function(video){
+			if(video.children) {
+				_.each(video.children, function(child){
+					_.each(child.resources, function(resource){
+						if(resource.height >= 720) {
+							globo.getDownloadURL(child, resource).then(function(url){
+								downloadManager.startDownload(url, video.title, child.title)
+									.then(function(){
+										//Finished
+									}, function(error){
+										console.log(error)
+									})
+							});
+						}
+					})
+				})
+
+			} else {
+				_.each(video.resources, function(resource){
+					if(resource.height >= 720) {
+						globo.getDownloadURL(video, resource).then(function(url){
+							downloadManager.startDownload(url, video.title, video.title)
+								.then(function(){
+									//Finished
+								}, function(error){
+									console.log(error)
+								})
+						});
+					}
+				})
+			}
+		})
+	})
